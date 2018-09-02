7cba05277d0626b4d43011c69dec45497a8f3dda
Klein <marco.klein94@gmail.com>
2018-09-02 10:05:09 +0200

    client network controller cleanup

--- src/client/ClientNetworkController.ts
+++ src/client/ClientNetworkController.ts
@@ -1,42 +1,9 @@
 import * as SocketIOClient from 'socket.io-client';
 import { NetworkController, Message, WorldUpdateData } from '../engine/Network';
 import { World, WorldController, WorldListener } from '../engine/World';
-import { ClientWorld } from './ClientWorld';
 import { ClientGameInterface } from './ClientMain';
 import { PlayerControl } from './ClientRoles';
 import { DataNode } from '../engine/Dataframework';
-//const io = require('socket.io');
-
-
-export interface ClientMessage extends Message {
-  apply(data: any, world: ClientWorld): void;
-
-}
-
-export abstract class WorldMessage implements ClientMessage {
-  data: any;
-  type: string;
-
-  constructor(data?: any) {
-    this.data = data;
-  }
-
-  abstract apply(data: any, world: ClientWorld): void;
-}
-
-export namespace WorldMessages {
-  export const WORLD_MESSAGE_PREFIX: string = 'WM.';
-  export class AddEntity extends WorldMessage {
-    static TYPE: string = WORLD_MESSAGE_PREFIX + 'AE';
-    type = AddEntity.TYPE;
-
-    apply(data: any, world: ClientWorld): void {
-      console.log('Adding the entity: %s with data ', data.type, data.data);
-      world.addEntity(world.entityFactory.produceFromType(data.type, data.data), data.data.id);
-    }
-
-  }
-}
 
 export class ClientNetworkController extends NetworkController implements WorldController {
   private socket: SocketIOClient.Socket;
@@ -68,12 +35,8 @@ export class ClientNetworkController extends NetworkController implements WorldC
       this.applyWorldChanges(changes);
     });
 
-    this.registerWorldMessages();
   }
 
-  private registerWorldMessages() {
-    this.registerMessage(WorldMessages.AddEntity.TYPE, WorldMessages.AddEntity);
-  }
 
   private setEntityId(entityId: string) {
     this.entityId = entityId;
@@ -134,18 +97,11 @@ export class ClientNetworkController extends NetworkController implements WorldC
 
   onMessage(message: any) {
     console.log('Recieved event!', message);
-    if (message.type === WorldMessages.AddEntity.TYPE) {
-      console.log('Recieved add entity message!');
-      //new WorldMessages.AddEntity(message.data).apply(message.data, this.game.world);
-    } else if (message.type === 'Handshake') {
+    if (message.type === 'Handshake') {
       console.log('Recieved Handshake message:' + message.data);
       this.clientId = message.data.clientId;
-    } else if (message.type === 'Data.U') {
-      // entity data updated
-      this.game.world.getEntityById(message.data.nodeId).data(message.data.key, message.data.value);
-    } else if (message.type === 'Data.D') {
-      // entity data deleted
-      this.game.world.getEntityById(message.data.nodeId).data(message.data.key, undefined);
+    } else {
+      console.error('Unhandled message with type %s', message.type);
     }
   }
 
