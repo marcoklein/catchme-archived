9703297084a6e5843900aedbc289e31e7bc092d0
Klein <marco.klein94@gmail.com>
2018-09-02 10:02:23 +0200

    removed game folder and merged game roles, entity factory with client

--- src/client/ClientEntityProducers.ts
+++ src/client/ClientEntityProducers.ts
@@ -1,15 +1,25 @@
 import { EntityProducer } from "../engine/EntityFactory";
 import { Role, DataNode } from "../engine/Dataframework";
-import * as Roles from "../game/Roles";
 import * as ClientRoles from "./ClientRoles";
 
+
+export class SimpleSpriteProducer implements EntityProducer {
+  produceEntity(type: string, data: any): DataNode {
+    let node = new DataNode();
+
+    node.addRole(new ClientRoles.SpriteRole(data.x, data.y, data.image));
+
+    return node;
+  }
+
+}
 
 export class PlayerProducer implements EntityProducer {
   produceEntity(type: string, data: any): DataNode {
     let node = new DataNode(data);
 
     node.addRole(new ClientRoles.PlayerControl());
-    node.addRole(new Roles.SpriteRole());
+    node.addRole(new ClientRoles.SpriteRole());
 
     return node;
   }
